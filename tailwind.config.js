c762d3d01857a941c6749c843520b11d4774f4c5
JasonRavager <vergara.ecjr@gmail.com>
2024-06-22 16:38:26 +0800

    finish the mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        nutmeg: "hsl(14, 45%, 36%)",
+        darkraspberry: "hsl(332, 51%, 32%)",
+        rosewhite: "hsl(330, 100%, 98%)",
+        eggshell: "hsl(30, 54%, 90%)",
+        lightgrey: "hsl(30, 18%, 87%)",
+        wengebrown: "hsl(30, 10%, 34%)",
+        darkcharcoal: "hsl(24, 5%, 18%)",
+      },
+      fontFamily: {
+        youngserif: ["'Young Serif'", "serif"],
+        outfit: ["Outfit", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
